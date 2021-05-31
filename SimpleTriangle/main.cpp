ec77def1342cbfbc5c4a3460969021ecfa2f4430
yuri kozlovskij <ykozlovskiy@virtido.com>
2021-05-31 11:17:21 +0300

    hf

--- SimpleTriangle/main.cpp
+++ SimpleTriangle/main.cpp
@@ -138,7 +138,7 @@ void setup_textures()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
     int width, height, nrChannels;
-    /*unsigned char* data = stbi_load(std::experimental::filesystem::path().c_str(), &width, &height, &nrChannels, 0);
+    unsigned char* data = stbi_load(std::experimental::filesystem::path("/wall.jpg"), &width, &height, &nrChannels, 0);
     if (data)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
@@ -149,7 +149,7 @@ void setup_textures()
         std::cout << "Failed to load texture" << std::endl;
     }
 
-    stbi_image_free(data);*/
+    stbi_image_free(data);
 }
 
 int main()
